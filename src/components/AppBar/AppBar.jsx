add40e959293f4624c7cbf183bf1ea4ac707c735
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-12-15 11:51:09 +0100

    new folder

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import s from "./AppBar.module.css";
+import Navigation from "../Navigation/Navigation";
+import AuthNav from "../AuthNav/AuthNav";
 
 const AppBar = () => {
-  return <div>AppBar</div>;
+  return (
+    <header className={s.header}>
+      <Navigation />
+      <AuthNav />
+    </header>
+  );
 };
 
 export default AppBar;
