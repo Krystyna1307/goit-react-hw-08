5c37660a872d007b921f2a718bc28a7259272ac7
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-12-17 10:01:19 +0100

    selectFilter

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,13 +1,1 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { selectContacts } from "../contacts/selectors";
-
 export const selectFilter = (state) => state.filters.filters.name;
-
-export const selectFilteredContacts = createSelector(
-  [selectContacts, selectFilter],
-  (contacts, filter) => {
-    return contacts.filter((item) =>
-      item.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-);
